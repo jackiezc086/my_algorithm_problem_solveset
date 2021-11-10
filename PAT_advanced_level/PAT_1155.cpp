484f64b733ccf0733197cc6127b93a439a0e2f9a
Zhouliang Cai <574970687@qq.com>
2021-11-10 14:55:20 +0800

    PAT_1155 fixed

--- PAT_advanced_level/PAT_1155.cpp
+++ PAT_advanced_level/PAT_1155.cpp
@@ -1,51 +1,36 @@
-//dfs之前写成死循环了，改了好久才改好
-//todo 以后应把层数加入dfs的形参
 #include <iostream>
 #include <cmath>
 #include <vector>
 using namespace std;
 int const N = 1005;
 int h[N];
-int num;
+int n;
 bool max_heap = true;
 bool min_heap = true;
-int seq[500][10];//seq[i][j] represents the j^th element in i^th sequence
+vector<int> path;
 
 void dfs(int x){
-    if(2 * x > num){
-        vector<int> res;
-        int y = x;
-        while (y != 1){
-            res.push_back(y);
-            if(y % 2 == 0) y /= 2;
-            else if(y % 2 == 1) y = (y - 1) / 2;
-        }
-        res.push_back(1);
-        bool is_first = true;
-        vector<int> seq;//判断是否递增或递减
-        while(!res.empty()){
-            if(is_first){
-                is_first = false;
-            }
-            else cout << ' ';
-            seq.push_back(h[res.back()]);
-            cout << h[res.back()];
-            res.pop_back();
-        }
-        for(int i = 1; i < seq.size(); i ++){
-            if(seq[i] > seq[i - 1]) max_heap = false;
-            if(seq[i] < seq[i - 1]) min_heap = false;
+    path.push_back(h[x]);
+    
+    if(2 * x > n){//叶节点
+        cout << path[0];
+        for(int i = 1; i < path.size(); i ++){
+            if(path[i] > path[i - 1]) max_heap = false;
+            if(path[i] < path[i - 1]) min_heap = false;
+            cout << ' ' << path[i];
         }
         cout << endl;
-        return;
     }
-    if(2 * x + 1 <= num) dfs(2 * x + 1);
-    if(2 * x <= num) dfs(2 * x);
+    
+    if(2 * x + 1 <= n) dfs(2 * x + 1);
+    if(2 * x <= n) dfs(2 * x);
+    
+    path.pop_back();
 }
 
 int main(){
-    cin >> num;
-    for(int i = 1; i <= num; i ++){
+    cin >> n;
+    for(int i = 1; i <= n; i ++){
         cin >> h[i];
     }
     
@@ -57,4 +42,3 @@ int main(){
     if (max_heap && min_heap)puts("Error");
     return 0;
 }
-
